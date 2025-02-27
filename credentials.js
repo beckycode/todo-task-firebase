4bb11dcdd643fa4baeadb30cb716d978ca583534
Rebeca González <hi.beckycode@gmail.com>
2025-02-26 23:12:29 -0500

    fix: another config

--- credentials.js
+++ credentials.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: window.env.APP_FIREBASE_API_KEY,
-  authDomain: window.env.APP_FIREBASE_AUTH_DOMAIN,
-  projectId: window.env.APP_PROJECT_ID,
-  storageBucket: window.env.APP_STORAGE_BUCKET,
-  messagingSenderId: window.env.APP_MESSAGING_SENDER_ID,
-  appId: window.env.APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 export { firebaseConfig };
