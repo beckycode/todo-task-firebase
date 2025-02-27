13ec0197511313008632c8cace20759084fa27e0
Rebeca González <hi.beckycode@gmail.com>
2025-02-26 22:44:24 -0500

    fix: add files to deploy to netlify

--- credentials.js
+++ credentials.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.APP_FIREBASE_API_KEY,
-  authDomain: process.env.APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.APP_PROJECT_ID,
-  storageBucket: process.env.APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.APP_MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: window.env.APP_FIREBASE_API_KEY,
+  authDomain: window.env.APP_FIREBASE_AUTH_DOMAIN,
+  projectId: window.env.APP_PROJECT_ID,
+  storageBucket: window.env.APP_STORAGE_BUCKET,
+  messagingSenderId: window.env.APP_MESSAGING_SENDER_ID,
+  appId: window.env.APP_ID,
 };
 
 export { firebaseConfig };
